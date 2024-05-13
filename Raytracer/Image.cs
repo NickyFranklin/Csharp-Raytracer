1c39231c3fc692c6d558137175dc366ae36db3d3
NickyFranklin <114303762+NickyFranklin@users.noreply.github.com>
2024-05-13 15:13:09 -0400

    Added File writing system for ppm files

---
+++ Raytracer/Image.cs
@@ -0,0 +1,18 @@
+public class Pixel {
+    public int r;
+    public int g;
+    public int b;
+
+    public Pixel() {
+        this.r = 255;
+        this.g = 255;
+        this.b = 255;
+    }
+
+    public Pixel(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
+
---
+++ Raytracer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //set initial values for image width and height if none provided
+        int width = 512;
+        int height = 512;
+
+        //If values are provided, set the values
+        if (args.Length == 2) {
+            width = int.Parse(args[0]);
+            height = int.Parse(args[1]);
+        }
+
+        //verify values set
+        Console.WriteLine( width + " " + height);
+
+        //create file with a name
+        File.WriteAllText("Pic.ppm", "P3\n");
+        File.AppendAllText("Pic.ppm", $"{width} {height}\n");
+        File.AppendAllText("Pic.ppm", "255\n");
+
+        Pixel[] pixelArr = new Pixel[width * height];
+
+        //Write Pixel Data loop
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                pixelArr[j+(i*width)] = new Pixel(120, 120, 120);
+            }
+        }
+
+        //Write to file loops
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                if(j != (width - 1)) {
+                    File.AppendAllText("Pic.ppm", 
+                    $"{pixelArr[j+(i*width)].r} {pixelArr[j+(i*width)].g} {pixelArr[j+(i*width)].b} ");
+                }
+
+                else {
+                    File.AppendAllText("Pic.ppm", 
+                    $"{pixelArr[j+(i*width)].r} {pixelArr[j+(i*width)].g} {pixelArr[j+(i*width)].b}\n");
+                }
+            }
+        }
+
+    }
+} 
